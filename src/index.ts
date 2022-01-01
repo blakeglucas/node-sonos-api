245f39df0c8732b6e46c32441b3955bf1659b0d7
Blake Lucas <blakeglucas@gmail.com>
2021-12-31 22:51:03 -0600

    Added an audio source action

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export { SonosAPI } from './SonosAPI'
 export { SonosDevice } from './SonosDevice'
 import * as types from './types'
 export { types }
+import * as actions from './actions'
+export { actions }
